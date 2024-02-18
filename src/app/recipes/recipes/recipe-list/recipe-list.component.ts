fa3ed39a95e8e9c8bdc2c7e694c703a25d12585f
jormarcus <jormarcus92@gmail.com>
2024-02-18 10:19:52 -0500

    feat(recipe-book): recipe list

--- src/app/recipes/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Parmesan',
+      'A delicious Italian dish',
+      'https://thecozycook.com/wp-content/uploads/2022/08/Chicken-Parmesan-Recipe-f-500x500.jpg'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
